bda76d3b0bdee6bd30375a03c68dcafe75986bb9
jykim-physics <245d57634@gmail.com>
2022-09-08 18:59:13 +0900

    0908

--- hist/get_data.py
+++ hist/get_data.py
@@ -12,22 +12,22 @@ import seaborn as sns
 #%matplotlib inline
 #px = 1/plt.rcParams['figure.dpi']
 
-def get_data(file='file', sample=None, tree='tree', base_filter=None,variables=None):
 
+
+def get_data(file='file', figname='name', tree='tree', base_filter=None,variables=None):
+    
     tree=tree
     f = file
-    base_filter  = base_filter
-
-    ROOT_df_start_0 = ROOT.RDataFrame(tree, f)
-    ROOT_df_start   = ROOT_df_start_0.Filter('Dstarp_isSignal!=1')
-
+    base_filter  = base_filter    
+    
+    ROOT_df_start = ROOT.RDataFrame(tree, f)  
+    
+    col_dict  = ROOT_df_start.AsNumpy(variables)
+    
     if base_filter !=None:
-        ROOT_df_filtered  = ROOT_df_start.Filter(base_filter)
+        ROOT_df_filtered  = ROOT_df_start.Filter(base_filter)                            
         col_dict  = ROOT_df_filtered.AsNumpy(variables)
-    else:
-        col_dict  = ROOT_df_start.AsNumpy(variables)
-
+    
     pd_df  = pd.DataFrame(col_dict)
-    pd_df['class'] = sample
-
+    
     return pd_df
